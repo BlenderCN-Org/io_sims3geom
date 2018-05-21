f0371eb1794f9848b0088a5758ceab311cbd98f4
SmugTomato <smugtomato@gmail.com>
2018-05-21 21:23:26 +0200

    Added data reader and writer

--- io_sims3geom/__init__.py
+++ io_sims3geom/__init__.py
@@ -16,5 +16,3 @@ Created by SmugTomato
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-
-## Testing file
