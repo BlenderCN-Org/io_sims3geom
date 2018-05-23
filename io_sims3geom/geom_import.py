aeaaaae4e072c61289a7221f759e41696d253bd0
SmugTomato <smugtomato@gmail.com>
2018-05-24 00:40:41 +0200

    Read file from Blender

--- io_sims3geom/geom_import.py
+++ io_sims3geom/geom_import.py
@@ -21,6 +21,8 @@ from bpy_extras.io_utils import ImportHelper
 from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 
+from .rcol.geom import Geom
+
 
 class ImportGeom(Operator, ImportHelper):
     """Sims 3 GEOM Importer"""
@@ -36,21 +38,20 @@ class ImportGeom(Operator, ImportHelper):
             maxlen=255,  # Max internal buffer length, longer would be clamped.
             )
 
-    # List of operator properties, the attributes will be assigned
-    # to the class instance from the operator settings before calling.
-    use_setting = BoolProperty(
-            name="Example Boolean",
-            description="Example Tooltip",
-            default=True,
-            )
-
-    type = EnumProperty(
-            name="Example Enum",
-            description="Choose between two items",
-            items=(('OPT_A', "First Option", "Description one"),
-                   ('OPT_B', "Second Option", "Description two")),
-            default='OPT_A',
+    use_strict = BoolProperty(
+            name="Strict",
+            description="Stop import process when reading unexpected TGI value(s) in header",
+            default=False,
             )
 
     def execute(self, context):
-        return read_some_data(context, self.filepath, self.use_setting)
+        geom = Geom.from_file(self.filepath)
+        if not geom:
+            return {'CANCELLED'}
+        elif not geom.read_data(strict=self.use_strict):
+            print("\nCancelled at", geom.reader.offset, "/", len(geom.reader.data))
+            return {'CANCELLED'}
+
+        # Do stuff here
+
+        return {'FINISHED'}
